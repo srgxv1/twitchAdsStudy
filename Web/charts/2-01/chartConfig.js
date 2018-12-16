162f344f5363f98534a09dc1c01091d5ada6c510
Sergio <srgxv@outlook.es>
2018-12-16 18:52:45 +0100

    added 1st chart

---
+++ Web/charts/2-01/chartConfig.js
@@ -0,0 +1,42 @@
+var api_key = 'rshnphwnqrnhfkxpzjrc'
+      axios({
+        method: 'post',
+        url: 'https://api.flex.io/v1/me/pipes/tw_ads_stdy_2_01/run',
+        headers: {
+          Authorization: 'Bearer ' + api_key
+        }
+      })
+      .then(response => {
+        var ctx = document.getElementById('chartCanvas').getContext('2d')
+        window.my_chart = new Chart(ctx, {
+          type: 'bar',
+          data: response.data,
+          options: 
+          {
+            legend: {
+                labels: {
+                    fontColor: "white",
+                    fontSize: 18
+                }
+            },
+            scales: 
+            {
+                xAxes: 
+                [{
+                    ticks: {
+                        display : false
+                    }
+                }],
+                yAxes: 
+                [{
+                    ticks: {
+                        fontColor: "white"
+                    }
+                }],
+            },
+            
+        }
+        })
+      })
+      .catch(response => {
+      })
