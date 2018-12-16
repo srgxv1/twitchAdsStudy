df876ae239f3026abca417539d9c17f54070ea15
Unknown <rodrigo.manuel.perez@gmail.com>
2018-12-16 12:31:12 +0100

    Scripts para hora y dia separados

---
+++ Scripts/ScriptsDias/6Proyecto_spark.py
@@ -0,0 +1,15 @@
+from pyspark import SparkConf, SparkContext, SQLContext, Row
+from pyspark.sql.functions import col
+import sys
+
+conf = SparkConf().setMaster('local').setAppName('Twitch')
+sc = SparkContext(conf = conf)
+sqlContext = SQLContext(sc)
+
+inputRDD = sc.textFile('FullDay6').map(lambda x: x.split(','))
+
+ids = inputRDD.map(lambda p: Row(Game = p[0].lower(), AllViewers = p[1]))
+
+df = ids.toDF()
+
+df.groupBy(col("Game")).agg({"AllViewers":"sum"}).orderBy("sum(AllViewers)", ascending=False).limit(10).coalesce(1).write.format("com.databricks.spark.csv").save('Day6')
