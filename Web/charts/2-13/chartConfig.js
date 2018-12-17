3260c9c03461509b0f0bd38b6a405e846073da06
Sergio <srgxv@outlook.es>
2018-12-17 19:03:29 +0100

    all charts added

---
+++ Web/charts/2-13/chartConfig.js
@@ -0,0 +1,44 @@
+var api_key = 'rshnphwnqrnhfkxpzjrc'
+      axios({
+        method: 'post',
+        url: 'https://api.flex.io/v1/me/pipes/tw_ads_stdy_2_13/run',
+        headers: {
+          Authorization: 'Bearer ' + api_key
+        }
+      })
+      .then(response => {
+        var ctx = document.getElementById('13Feb').getContext('2d')
+        ctx.height = 100
+        window.my_chart = new Chart(ctx, {
+          type: 'bar',
+          data: response.data,
+          options: 
+          {
+            responsive: true,
+            legend: {
+                labels: {
+                    fontColor: "white",
+                    fontSize: 18
+                }
+            },
+            scales: 
+            {
+                xAxes: 
+                [{
+                    ticks: {
+                        display : false
+                    }
+                }],
+                yAxes: 
+                [{
+                    ticks: {
+                        fontColor: "white"
+                    }
+                }],
+            },
+            
+        }
+        })
+      })
+      .catch(response => {
+      })
